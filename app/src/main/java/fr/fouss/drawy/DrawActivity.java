61c35caaef2c2ca2aed685c46773606697eb9f04
MrFouss <maxime.brodat@fouss.fr>
2017-11-21 18:42:22 -0500

    Add color picker

--- app/src/main/java/fr/fouss/drawy/DrawActivity.java
+++ app/src/main/java/fr/fouss/drawy/DrawActivity.java
@@ -1,5 +1,6 @@
 package fr.fouss.drawy;
 
+import android.content.DialogInterface;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
@@ -8,6 +9,12 @@ import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
+import android.widget.Toast;
+
+import com.flask.colorpicker.ColorPickerView;
+import com.flask.colorpicker.OnColorSelectedListener;
+import com.flask.colorpicker.builder.ColorPickerClickListener;
+import com.flask.colorpicker.builder.ColorPickerDialogBuilder;
 
 public class DrawActivity extends AppCompatActivity {
 
@@ -15,6 +22,8 @@ public class DrawActivity extends AppCompatActivity {
     private MenuItem toolbarThickness;
     private MenuItem toolbarShape;
     private MenuItem toolbarBrush;
+
+    private int drawingColor;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -25,6 +34,8 @@ public class DrawActivity extends AppCompatActivity {
         setSupportActionBar(toolbar);
         assert getSupportActionBar() != null;
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+
+        drawingColor = 0xffffffff;
     }
 
     /**
@@ -52,6 +63,34 @@ public class DrawActivity extends AppCompatActivity {
     public boolean onOptionsItemSelected(MenuItem item) {
         switch (item.getItemId()) {
             case R.id.colorButton :
+                ColorPickerDialogBuilder
+                        .with(this)
+                        .setTitle("Choose a color")
+                        .initialColor(drawingColor)
+                        .wheelType(ColorPickerView.WHEEL_TYPE.CIRCLE)
+                        .density(10)
+                        .showAlphaSlider(false)
+                        .showLightnessSlider(true)
+                        .showColorEdit(false)
+                        .showColorPreview(false)
+                        .setOnColorSelectedListener(new OnColorSelectedListener() {
+                            @Override
+                            public void onColorSelected(int selectedColor) {
+                            }
+                        })
+                        .setPositiveButton("Ok", new ColorPickerClickListener() {
+                            @Override
+                            public void onClick(DialogInterface dialog, int selectedColor, Integer[] allColors) {
+                                Toast.makeText(DrawActivity.this, "setDrawingColor: 0x" + Integer.toHexString(selectedColor), Toast.LENGTH_SHORT).show();
+                                drawingColor = selectedColor;
+                            }
+                        })
+                        .setNegativeButton("Cancel", new DialogInterface.OnClickListener() {
+                            @Override
+                            public void onClick(DialogInterface dialog, int which) {}
+                        })
+                        .build()
+                        .show();
                 return true;
             case R.id.thicknessButton :
                 return true;
