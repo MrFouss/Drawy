b2c1699d1b43765b0bb09e67b6edb5307295a77c
Ezia <esia.belbachir@protonmail.com>
2017-11-26 22:59:59 -0500

    Implement support of images in DrawView

--- app/src/main/java/fr/fouss/drawy/DrawView.java
+++ app/src/main/java/fr/fouss/drawy/DrawView.java
@@ -3,45 +3,81 @@ package fr.fouss.drawy;
 import android.content.Context;
 import android.graphics.Bitmap;
 import android.graphics.Canvas;
+import android.graphics.Matrix;
 import android.graphics.Paint;
 import android.graphics.Path;
 import android.util.AttributeSet;
 import android.util.DisplayMetrics;
-import android.util.Log;
-import android.util.TypedValue;
 import android.view.MotionEvent;
 import android.view.ScaleGestureDetector;
 import android.view.View;
 
 public class DrawView extends View {
+
+    ///// FIELDS /////
+
+    ///// GENERAL /////
+
     private Bitmap drawing;
     private Canvas canvas;
     private Paint paint;
-    private Mode mode;
+    private Mode mode = Mode.BRUSH;
+    private float lastX = -1;
+    private float lastY = -1;
 
-    private float lastX, lastY;
-    private Path brushPath;
-    private boolean validPath;
-
-    private Shape currShape;
-    private float shapeScaleX, shapeScaleY, shapeScale;
-    private float shapeX, shapeY;
-    private boolean pointer1Down, pointer2Down;
-    private int pointer1Id, pointer2Id;
-    private boolean scaling;
-    private int fingerNbr;
-
-    public enum Mode {BRUSH, SHAPE}
-    public enum Shape {CIRCLE, SQUARE}
-
+    ///// BRUSH /////
+    private Path brushPath = new Path();
     private static final float TOUCH_TOLERANCE = 4;
 
+    private boolean validPath = false;
+
+    ///// SHAPE / IMAGE /////
+    private Shape currShape = Shape.CIRCLE;
+    private Bitmap currImage = null;
+    private float shapeX;
+    private float shapeY;
+    private float shapeScale = 1;
+    private int pointer1Id = -1;
+    private boolean scaling = false;
     private ScaleGestureDetector scaleDetector;
+
+    ///// ENUMS /////
+
+    public enum Mode {BRUSH, SHAPE, IMAGE}
+
+    public enum Shape {CIRCLE, SQUARE}
+
+    ///// CONSTRUCTOR /////
 
     public DrawView(Context context, AttributeSet attrs) {
         super(context, attrs);
         initView(context);
     }
+
+    private void initView(Context context) {
+        DisplayMetrics metrics = context.getResources().getDisplayMetrics();
+        int width = metrics.widthPixels;
+        int height = metrics.heightPixels;
+
+        drawing = Bitmap.createBitmap(width, height, Bitmap.Config.ARGB_4444);
+        drawing.eraseColor(0xffffffff);
+
+        paint = new Paint();
+        paint.setColor(0xff000000);
+        paint.setStrokeWidth(20.0f);
+        paint.setStrokeCap(Paint.Cap.ROUND);
+        paint.setStrokeJoin(Paint.Join.ROUND);
+        paint.setStyle(Paint.Style.STROKE);
+
+        canvas = new Canvas(drawing);
+
+        shapeX = width/2;
+        shapeY = height/2;
+
+        scaleDetector = new ScaleGestureDetector(context, new ScaleListener());
+    }
+
+    ///// GENERAL /////
 
     public void resetCanvas(int color) {
         drawing.eraseColor(color);
@@ -67,9 +103,25 @@ public class DrawView extends View {
         return (int) paint.getStrokeWidth();
     }
 
+    public Shape getShape() {
+        return currShape;
+    }
+
+    public void setShape(Shape shape) {
+        this.currShape = shape;
+    }
+
+    public Bitmap getImage() {
+        return currImage;
+    }
+
+    public void setImage(Bitmap image) {
+        this.currImage = image;
+    }
+
     public void setMode(Mode mode) {
         this.mode = mode;
-        if (mode == Mode.SHAPE) {
+        if (mode == Mode.SHAPE || mode == Mode.IMAGE) {
             shapeX = canvas.getWidth()/2;
             shapeY = canvas.getHeight()/2;
         }
@@ -79,55 +131,14 @@ public class DrawView extends View {
         return this.mode;
     }
 
-    public void anchorShape() {
+    ///// DRAW METHODS /////
+
+    public void anchor() {
         if (mode == Mode.SHAPE) {
             drawShape(canvas);
+        } else if (mode == Mode.IMAGE) {
+            drawImage(canvas);
         }
-    }
-
-    private void initView(Context context) {
-        DisplayMetrics metrics = context.getResources().getDisplayMetrics();
-        int width = metrics.widthPixels;
-        int height = metrics.heightPixels;
-
-        drawing = Bitmap.createBitmap(width, height, Bitmap.Config.ARGB_4444);
-        drawing.eraseColor(0xffffffff);
-
-        paint = new Paint();
-        paint.setColor(0xff000000);
-        paint.setStrokeWidth(20.0f);
-        paint.setStrokeCap(Paint.Cap.ROUND);
-        paint.setStrokeJoin(Paint.Join.ROUND);
-        paint.setStyle(Paint.Style.STROKE);
-
-        canvas = new Canvas(drawing);
-
-        lastX = -1;
-        lastY = -1;
-
-        setMode(Mode.BRUSH);
-
-        currShape = Shape.CIRCLE;
-        shapeScaleX = 1;
-        shapeScaleY = 1;
-        shapeX = width/2;
-        shapeY = height/2;
-        shapeScale = 1;
-
-        scaleDetector = new ScaleGestureDetector(context, new ScaleListener());
-
-        brushPath = new Path();
-
-        pointer1Down = false;
-        pointer2Down = false;
-
-        pointer1Id = -1;
-        pointer2Id = -1;
-
-        scaling = false;
-        fingerNbr = 0;
-
-        validPath = false;
     }
 
     @Override
@@ -137,6 +148,7 @@ public class DrawView extends View {
         canvas.drawPath(brushPath, paint);
         canvas.drawPoint(lastX, lastY, paint);
         drawShape(canvas);
+        drawImage(canvas);
     }
 
     private void drawShape(Canvas canvas) {
@@ -145,23 +157,31 @@ public class DrawView extends View {
                 case CIRCLE:
                     canvas.drawCircle(shapeX, shapeY, 150*shapeScale, paint);
                 case SQUARE:
-                    canvas.drawRect(-150, -150, 150, 150, paint);
+                    canvas.drawRect(shapeX-150*shapeScale, shapeY-150*shapeScale,
+                            shapeX+150*shapeScale, shapeY+150*shapeScale, paint);
             }
         }
     }
 
+    private void drawImage(Canvas canvas) {
+        if (mode == Mode.IMAGE && currImage != null) {
+            // TODO debug transform
+            Matrix transform = new Matrix();
+            transform.postTranslate(shapeX-currImage.getWidth()/2,
+                    shapeY-currImage.getHeight()/2);
+            transform.postScale(shapeScale, shapeScale, shapeX, shapeY);
+            canvas.drawBitmap(currImage, transform, paint);
+        }
+    }
+
+    ///// EVENTS /////
+
     @Override
     public boolean onTouchEvent(MotionEvent event) {
-//        Log.d("DrawView", "onTouchEvent: drawing type : "
-//                + event.getAction()
-//                + " ; position : "
-//                + event.getX()
-//                + ";"
-//                + event.getY());
         if (mode == Mode.BRUSH) {
             return onTouchEventBrush(event);
-        } else if (mode == Mode.SHAPE) {
-            return onTouchEventShape(event);
+        } else if (mode == Mode.SHAPE || mode == Mode.IMAGE) {
+            return onTouchEventShapeAndImage(event);
         } else {
             return super.onTouchEvent(event);
         }
@@ -192,13 +212,11 @@ public class DrawView extends View {
         } else if (event.getAction() == MotionEvent.ACTION_UP) {
             float x = event.getX();
             float y = event.getY();
-            float dx = Math.abs(x - lastX);
-            float dy = Math.abs(y - lastY);
             brushPath.quadTo(lastX, lastY, (x + lastX)/2, (y + lastY)/2);
-            canvas.drawPath(brushPath, paint);
             if (!validPath) {
                 canvas.drawPoint(x, y, paint);
             } else {
+                canvas.drawPath(brushPath, paint);
                 validPath = false;
             }
             brushPath.reset();
@@ -211,15 +229,15 @@ public class DrawView extends View {
         }
     }
 
-    public boolean onTouchEventShape(MotionEvent event) {
+    public boolean onTouchEventShapeAndImage(MotionEvent event) {
         int index = event.getActionIndex();
         int id = event.getPointerId(index);
 
+        // check if in scaling mode
         if (event.getPointerCount() > 1) {
-//            Log.i("DrawView", "Start scaling");
             scaling = true;
         } else if (event.getActionMasked() == MotionEvent.ACTION_UP) {
-//            Log.i("DrawView", "Stop scaling");
+            // if the last finger is lifted update shape position/toggle false scaling
             if (!scaling) {
                 lastX = shapeX;
                 lastY = shapeY;
@@ -229,14 +247,12 @@ public class DrawView extends View {
         }
 
         if (scaling) {
-//            Log.i("DrawView", "Scaling");
             shapeX = lastX;
             shapeY = lastY;
             scaleDetector.onTouchEvent(event);
             invalidate();
             return true;
         } else if (event.getActionMasked() == MotionEvent.ACTION_DOWN) {
-//            Log.i("DrawView", "Init move");
             pointer1Id = id;
             float x = event.getX(pointer1Id);
             float y = event.getY(pointer1Id);
@@ -246,15 +262,12 @@ public class DrawView extends View {
             shapeY = y;
             invalidate();
             return true;
-        } else if (event.getActionMasked() == MotionEvent.ACTION_MOVE) {
-            if (id == pointer1Id) {
-//                Log.i("DrawView", "Move");
-                float x = event.getX(pointer1Id);
-                float y = event.getY(pointer1Id);
-                shapeX = x;
-                shapeY = y;
-                invalidate();
-            }
+        } else if (event.getActionMasked() == MotionEvent.ACTION_MOVE && id == pointer1Id) {
+            float x = event.getX(pointer1Id);
+            float y = event.getY(pointer1Id);
+            shapeX = x;
+            shapeY = y;
+            invalidate();
             return true;
         }
 
@@ -262,19 +275,8 @@ public class DrawView extends View {
     }
 
     private class ScaleListener extends ScaleGestureDetector.SimpleOnScaleGestureListener {
-
         @Override
         public boolean onScale(ScaleGestureDetector detector) {
-            if (detector.getPreviousSpanX() == 0) {
-                shapeScaleX *= 1;
-            } else {
-                shapeScaleX *= detector.getCurrentSpanX()/detector.getPreviousSpanX();
-            }
-            if (detector.getPreviousSpanY() == 0) {
-                shapeScaleY *= 1;
-            } else {
-                shapeScaleY *= detector.getCurrentSpanY()/detector.getPreviousSpanY();
-            }
             shapeScale *= detector.getScaleFactor();
             return true;
         }
